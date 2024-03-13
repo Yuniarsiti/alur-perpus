100ec84cfb67d782f148ee9371486f62d12b3319
yuniarsiti <yuniarsitinur@gmail.com>
2024-03-13 14:56:31 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>  
+ <div class="container-fluid">
+  <div class="row ny-5">
+   <div class="col-lg-6'>
+    <nuxt-link to="/pengunjung/tambah">
+     <div class="card bg-pengunjung rounded-5">
+      <div class="card-body">
+        <div class="card bg-pengunjung rounded-5">
+         <div class="card-body">
+          <h2>Pengunjung</h2>
+         </div>
+       </div>
+      <nuxt-link>
+     </div>
+
+     <div class="col-lg-6">
+      <nuxt-link to="/buku">
+       <div class="card-body">
+        <h2>Carl Buku</h2>
+         <div class="card bg-buku ronded-5>
+               

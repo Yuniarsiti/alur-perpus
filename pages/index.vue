f37e52cc6f3ad46ba0b20179f5091feee4728360
Yuniarsiti <141316968+Yuniarsiti@users.noreply.github.com>
2024-05-02 15:37:37 +0700

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,90 @@
 <template>
   <div class="container-fluid">
-    <div class="row">
-      <h2 class="text-center my-4">ISI BUKU KUNJUNGAN</h2>
-      <div class="col-lg-12">
-        <div class="mb-3">
-        <form>
-            <input type="text" class="form-control form control-lg rounded-5" placeholder="NAMA">
+    <div class="content">
+      <div class="row d-flex justify-content-center">
+        <div class="col-lg-12">
+          <h2 class="text-center my-4 fw-bold font-inter">ISI BUKU KUNJUNGAN</h2>
+        </div>
+        <form class="card rounded p-5">
+          <div class="mb-3">
+            <input type="text" class="form-control form control-lg rounded-5" placeholder="Nama" />
+          </div>
           <div class="mb-3">
             <select class="form-control form-control-lg form-select rounded-5">
-              <option value="">KEANGGOTAAN</option>
-              <option value="siswa">SISWA</option>
-              <option value="guru">GURU</option>
-              <option value="staf">STAF</option>
-              <option value="umum">UMUM</option>
+              <option value="">Keanggotaan</option>
+              <option value="siswa">Siswa</option>
+              <option value="guru">Guru</option>
+              <option value="staf">Staf</option>
+              <option value="umum">Umum</option>
             </select>
           </div>
           <div class="mb-3">
             <div class="row">
-              <div class="col-mb-3">
-                <select class="form-control form-control-lg form select rounded-5 mb-2">
-                  <option value="">JURUSAN</option>
-                  <option value="PPLG">PPLG</option>
-                  <option value="TJKT">TJKT</option>
-                  <option value="TSM">TSM</option>
-                  <option value="DKV">DKV</option>
-                  <option value="TOI">TOI</option>
+              <div class="col-md-4">
+                <select class="form-control form control-lg form-select rounded-5 mb-2">
+                  <option value="">Tingkat</option>
+                  <option value="X">X</option>
+                  <option value="XI">XI</option>
+                  <option value="XII">XII</option>
                 </select>
               </div>
-              <div class="col-mb-3">
-                  <div class="col-mb-3">
-                    <select class="form-control form control-lg form-select rounded-5 mb-2">
-                      <option value="">TINGKAT</option>
-                      <option value="X">X</option>
-                      <option value="XI">XI</option>
-                      <option value="XII">XII</option>
-                      </select>
-                </div>
-                <div class="col-md-4">
-                </div><select class="form-control form-control-lg form-select rounded-5 mb-2">
-                        <option value="">KELAS</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                      </select>
-                    </div> 
-                  </div>
-                 </div>
-                 <div class="mb-3">
-                  <select class="form-control form control-lg form-select rounded-5">
-                    <option value="">KEPERLUAN</option>
-                    <option value="baca buku">BACA BUKU</option>
-                    <option value="pinjam buku">PINJAM BUKU</option>
-                    <option value="kembalikan buku">KEMBALIKAN BUKU</option>
-                  </select>
-                </div>
-                 <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">KIRIM</button>
-        </form>    
+              <div class="col-md-4">
+                <select class="form-control form-contol-lg form-select rounded-5 mb-2">
+                  <option value="">Jurusan</option>
+                  <option value="TJKT">TJKT</option>
+                  <option value="PPLG">PPLG</option>
+                  <option value="TOI">TOI</option>
+                  <option value="DKV">DKV</option>
+                  <option value="TBSM">TBSM</option>
+                </select>
+              </div>
+              <div class="col-md-4">
+                <select class="form-control form-control-lg form-select rounded-5 mb-2">
+                  <option value="">Kelas</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="mb-3">
+            <select class="form-control form control-lg form-select rounded-5">
+              <option value="">Keperluan</option>
+              <option value="baca buku">Baca buku</option>
+              <option value="pinjam buku">Pinjam buku</option>
+              <option value="kembalikan buku">Kembalikan buku</option>
+            </select>
+          </div>
+          <nuxt-link to="/pengunjung/">
+            <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">Kirim</button>
+          </nuxt-link>
+        </form>
       </div>
     </div>
   </div>
-</div>
-</template>    
+</template>
 
 <style scoped>
+.container-fluid {
+  background-color: #cfe8ef;
+}
 .card-body {
   width: 100%;
-  height:200em;
+  height: 20em;
   padding: 0;
 }
 .cover {
-  width:100%;
-  height:100%;
+  width: 10%;
+  height: 20%;
   object-fit: cover;
   object-position: 0 30;
+}
+.mb-3 {
+  font-size: 35px;
+}
+.btn {
+  margin-left: 90%;
 }
 </style>
---
+++ pages/pengunjung/riwayat.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+        <div class="my-3">
+          <input type="search" class="form-control fprm-control-lg rounded-5" placeholder="filter" />
+        </div>
+        <div class="my-3 text-muted">menampilkan 1 dari 2</div>
+        <table class="table">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>NAMA</td>
+              <td>KEANGGOTAAN</td>
+              <td>waktu</td>
+              <td>keperluan</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1.</td>
+              <td>yuniar</td>
+              <td>siswa</td>
+              <td>24 febuari 2024,23.31.00</td>
+              <td>Baca</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.text center {
+  font-family: Inria serif, serif;
+  font-size: 35px;
+  font-weight: 500px;
+  font-style: normal;
+}
+
+.btn {
+  background-color: #000000;
+  margin-left: 75%;
+}
+
+.form-control {
+  background-color: #cfe8ef;
+}
+}
+</style>
